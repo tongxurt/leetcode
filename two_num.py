27135cf7d797b9a511cef385ae0acd07af7609e9
tongxu <tongxu@bytedance.com>
2017-12-13 17:30:12 +0800

    add git ignore

--- two_num.py
+++ two_num.py
@@ -1,3 +1,13 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -8,7 +18,7 @@ class Solution(object):
         for first in nums:
             for sec in nums[nums.index(first) + 1:]:
                 if first + sec == target:
-                    return [nums.index(first), nums[nums.index(first) + 1:].index(sec) + nums.index(first)+1]
+                    return [nums.index(first), nums[nums.index(first) + 1:].index(sec) + nums.index(first) + 1]
 
         return []
 
